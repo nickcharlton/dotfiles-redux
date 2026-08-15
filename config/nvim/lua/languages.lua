6cf2d6d4feeef1a83f3f7a851e4342e61a93bbbf
Nick Charlton <nick@nickcharlton.net>
2026-08-15 16:59:00 +0100

    Setup rubocop/standardrb for Ruby
    This is based on Pablo's Lua to detect what it should be doing. A lot of
    projects have moved to `standardrb`, but this isn't guaranteed so it's
    helpful to keep supporting Rubocop directly.
    
    https://blog.pablobm.com/2026/01/17/neovim-lsp-stack/

--- config/nvim/lua/languages.lua
+++ config/nvim/lua/languages.lua
@@ -3,7 +3,9 @@ require("mason-lspconfig").setup({
   ensure_installed = {
     "lua_ls",
     "bashls",
-    "systemd_lsp"
+    "systemd_lsp",
+    "rubocop",
+    "standardrb"
   }
 })
 
@@ -34,6 +36,37 @@ vim.lsp.config("lua_ls", {
       }
     }
   }
+})
+
+local function rubocop_yml_expected(expecting_rubocop_yml)
+  return function(bufnr, on_dir)
+    -- Get the name of the current buffer. This will be a full path if a file
+    local file_path = vim.fn.bufname(bufnr)
+
+    -- Traverse directories up the hierarchy until one of Gemfile or .git is found.
+    -- This should be the root of the project.
+    local _root_dir = vim.fs.root(file_path, {"Gemfile", ".git"})
+
+    -- Is there a .rubocop.yml at the root?
+    local _rubocop_yml_present = not not (_root_dir and vim.uv.fs_stat(_root_dir .. "/.rubocop.yml"))
+
+    -- Assuming that a _root_dir was found
+    -- If there is a .rubocop.yml AND we expected it to be present, call on_dir().
+    -- If there is NO .rubocop.yml AND we did NOT expect it, also call on_dir().
+    -- Otherwise, do nothing.
+    if _root_dir and expecting_rubocop_yml == _rubocop_yml_present then
+      -- Notify that we found an appropriate root dir
+      on_dir(vim.fn.getcwd())
+    end
+  end
+end
+
+vim.lsp.config("rubocop", {
+  root_dir = rubocop_yml_expected(true)
+})
+
+vim.lsp.config("standardrb", {
+  root_dir = rubocop_yml_expected(false)
 })
 
 -- nvim-treesitter
